14c71c282d8968c56dd578f46b9c00bf4e5988f2
sebastian <ssantandersnw@gmail.com>
2023-05-30 00:59:19 -0400

    ocultar el valor del firebaseconfig

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/firestore'
 //import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCRil_9HhL2UwrcLGLG5ooT3RFKwls6VXQ",
-  authDomain: "medidor-agua-b5a83.firebaseapp.com",
-  databaseURL: "https://medidor-agua-b5a83-default-rtdb.firebaseio.com",
-  projectId: "medidor-agua-b5a83",
-  storageBucket: "medidor-agua-b5a83.appspot.com",
-  messagingSenderId: "483811688154",
-  appId: "1:483811688154:web:5d84bc4bd650a9252cefe3"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
